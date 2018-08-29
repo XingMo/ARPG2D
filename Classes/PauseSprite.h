615b738c4d3bcfa54dd33638176541c226362f59
HGH <603049648@qq.com>
2018-08-29 20:39:23 +0800

    添加地图场景

---
+++ Classes/PauseSprite.h
@@ -0,0 +1,24 @@
+#ifndef	_PAUSESPRITE__H_
+#define _PAUSESPRITE__H_
+
+#include "CommonData.h"
+#include "Level1Scene.h"
+#include "GameOverScene.h"
+
+USING_NS_CC;
+
+class  PauseScene: public Layer {
+public:
+     PauseScene();
+
+	~PauseScene();
+
+    static PauseScene* create();
+
+	static Scene* createScene(RenderTexture*);
+
+    bool init();
+
+};
+
+#endif
